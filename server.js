e4c0dbdee499b17320e084d9753e9e10ff37acd9
Patrick Williams <pwmckenna@gmail.com>
2012-10-30 03:26:31 -0700

    lofting a few stray variables

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var cconsole = colorize.console;
 var app = express();
 app.use(express.bodyParser());
 
+var request_image, resize_image;
+
 //memoizing here works as a cache...but its even better than that.
 //when you call for the second time with the same url, it doesn't
 //matter if the first request has resolved or not. Both requests will
@@ -19,14 +21,15 @@ app.use(express.bodyParser());
 //the caching is of the promise object, so dups of the results aren't
 //stored in memory, and the storage of the objects themselves is in some
 //tidy underscore closure
-var request_image = _.memoize(function(url) {
-    var ret = new async.Deferred();
-    var options = {
+request_image = _.memoize(function(url) {
+    var ret, options, request;
+    ret = new async.Deferred();
+    options = {
         encoding: 'binary',
         uri: url
     };
     //make the GET request with content type binary
-    var request = get(options);
+    request = get(options);
     //only resolve the return object once we've validated the response.
     request.then(function(result) {
         if(result.statusCode === 200) {
@@ -44,7 +47,7 @@ var request_image = _.memoize(function(url) {
 //as well. making underscore's memoize use these giant data blogs as lookup
 //keys is probably doubling the space needed for the same image, but the
 //speed boost is huge.
-var resize_image = _.memoize(function(data, width, height) {
+resize_image = _.memoize(function(data, width, height) {
     var ret = new async.Deferred();
     im.crop({
         srcData : data,
@@ -61,19 +64,20 @@ var resize_image = _.memoize(function(data, width, height) {
 });
 
 app.get('/', function (req, res) {
-    cconsole.log('#yellow[GET request]');
+    var width, height, image_url, image_request;
     //lets parse some args
-    var width = req.param('w');
-    var height = req.param('h');
-    var image_url = req.param('u');
+    width = req.param('w');
+    height = req.param('h');
+    image_url = req.param('u');
 
+    cconsole.log('#yellow[GET request]');
     cconsole.log(JSON.stringify([width, height, image_url], null, 4));
 
     //here's the course of action
     //1. grab remote image
     //2. resize it.
     //3. hand the resized image back to the original requestor
-    var image_request = request_image(image_url);
+    image_request = request_image(image_url);
     image_request.then(function(result) {
         var resize = resize_image(result.body, width, height);
         resize.then(function(stdout) {
