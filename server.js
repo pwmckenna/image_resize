67c8a6dd7ca8e15a085b4aa7e46787fdc444f10d
Patrick Williams <pwmckenna@gmail.com>
2012-10-30 02:42:08 -0700

    making a stupid resizer with memoized image request caching

--- server.js
+++ server.js
@@ -1,22 +1,76 @@
-var request = require('request')
-
+var promisify = require('./js/promisify');
+var request = require('request');
+var get = promisify(request.get);
+var async = require('promised-io/promise');
+var im = require("imagemagick");
 var express = require('express');
+var _ = require('underscore');
+
 var app = express();
 app.use(express.bodyParser());
 
-app.get('/', function(req, response){
-    var width, height, url;
-    width = req.param('w', null);
-    height = req.param('h', null);
-    url = req.param('u', null);
+//memoizing here works as a cache...but its even better than that.
+//when you call for the second time with the same url, it doesn't
+//matter if the first request has resolved or not. Both requests will
+//get the same promise object, which will be resolved at the same time.
+//so no timing issues to worry about...
+//the caching is of the promise object, so dups of the results aren't
+//stored in memory, and the storage of the objects themselves in in some
+//tidy underscore closure
+var request_image = _.memoize(function(url) {
+    var ret = new async.Deferred();
+    var options = {
+        encoding: 'binary',
+        uri: url
+    };
+    //make the GET request with content type binary
+    var request = get(options);
+    //only resolve the return object once we've validated the response.
+    request.then(function(result) {
+        if(result.statusCode === 200) {
+            ret.resolve(result);
+        } else {
+            ret.reject();
+        }
+    }, function() {
+        ret.reject();
+    });
+    return ret.promise;
+});
 
-    console.log('image request:');
-    console.log(JSON.stringify({
-        width: width,
-        height: height,
-        url: url
-    }, null, 4));
-    request.get(url).pipe(response);
+var resize_image = function(data, width, height) {
+    var ret = new async.Deferred();
+    im.resize({
+        srcData : data,
+        width : width,
+        height : height,
+    }, function(err, stdout, stderr) {
+        if(err) {
+            ret.reject(err);
+        } else {
+            ret.resolve(stdout);
+        }
+    });
+    return ret.promise;
+};
+
+app.get('/', function (req, res) {
+    var width = req.param('w');
+    var height = req.param('h');
+    var image_url = req.param('u');
+
+    var image_request = request_image(image_url);
+    image_request.then(function(result) {
+        var resize = resize_image(result.body, width, height);
+        resize.then(function(stdout) {
+            res.contentType(result.headers['content-type']);
+            res.end(stdout, 'binary');
+        }, function(err) {
+            res.send(500, { error: err });
+        });
+    }, function() {
+        res.send(404);
+    });
 });
 
 app.listen(3000);
